8195694290773e68e79d567a1c725a7e6561fb11
vdlindenmark <vdlindenmark@gmail.com>
2016-02-25 14:25:07 +0100

    messages Controller toegevoegd

--- chat2/config/routes.rb
+++ chat2/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/new'
+
+  get 'messages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
