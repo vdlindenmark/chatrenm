2aad64f2c19b5f363c04623741edaecf1f2fd188
vdlindenmark <vdlindenmark@gmail.com>
2016-02-26 13:24:05 +0100

    Redirect goed gezet! :D

--- chat2/app/controllers/messages_controller.rb
+++ chat2/app/controllers/messages_controller.rb
@@ -10,12 +10,11 @@ class MessagesController < ApplicationController
   	@message = Message.new(message_params)
 
   	@message.save
-  	redirect_to @message
+  	redirect_to :back
   end
 
   private
   	def message_params
     	params.require(:message).permit(:name, :content)
   	end
-
 end
