63d671f0fa6ec386833a56dbac5c7c5f4d7f0266
vdlindenmark <vdlindenmark@gmail.com>
2016-03-08 10:54:09 +0100

    Form op index gerepareerd

--- chat2/app/controllers/messages_controller.rb
+++ chat2/app/controllers/messages_controller.rb
@@ -15,6 +15,6 @@ class MessagesController < ApplicationController
 
   private
   	def message_params
-    	params.require(:message).permit(:name, :content)
+    	params.require(:messages).permit(:name, :content)
   	end
 end
